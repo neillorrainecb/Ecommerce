616235a70a9f02b7e9a644a1dc725cd2b131d8a8
Neil Lorraine Bundoc <neillorrainecb@gmail.com>
2023-10-21 12:34:52 +0800

    Remove to Ecommerce

---
+++ src/Pages/ShopCategory.jsx
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react'
+import './CSS/ShopCategory.css'
+import { ShopContext } from '../Context/ShopContext'
+
+const ShopCategory = (props) => {
+  const{all_products} = useContext(ShopContext);
+  return (
+    <div className='shop-category'>
+        <img src={props.banner}alt="" />
+    </div>
+  )
+}
+
+export default ShopCategory
