cabfd96fceaea822478a31da684ad725703aa6f2
Neil Lorraine Bundoc <neillorrainecb@gmail.com>
2023-10-21 20:19:03 +0800

    Add CSS LoginSignup page

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './CSS/LoginSignup.css'
 
 const LoginSignup = () => {
   return (
-    <div className='loginsingup'>
+    <div className='loginsignup'>
         <div className="loginsignup-container">
           <h1>Sign Up</h1>
           <div className="loginsignup-fields">
@@ -14,7 +15,7 @@ const LoginSignup = () => {
           <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
           <div className="loginsignup-agree">
             <input type="checkbox" name='' id='' />
-            <p>By continuing, i agree to the terms of use & privacy policy</p>
+            <p>By continuing, I agree to the terms of use & privacy policy</p>
           </div>
         </div>
     </div>
