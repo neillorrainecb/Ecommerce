7a1448b26a7eae640e268ea3bf8bf9999766f31b
Neil Lorraine Bundoc <neillorrainecb@gmail.com>
2023-10-23 22:43:27 +0800

    Product Description and Size CSS

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -7,7 +7,7 @@ import star_dull_icon from '../Assets/star_dull_icon.png'
 const ProductDisplay = (props) => {
   const {product} = props;
   return (
-    <div className='productDisplay'>
+    <div className='productdisplay'>
       <div className="productdisplay-left">
         <div className="productdisplay-img-list">
           <img src={product.image} alt="" />
@@ -21,7 +21,7 @@ const ProductDisplay = (props) => {
       </div>
       <div className="productdisplay-right">
         <h1>{product.name}</h1>
-        <div className="productdisplay-right-star">
+        <div className="productdisplay-right-stars ">
           <img src={star_icon} alt="" />
           <img src={star_icon} alt="" />
           <img src={star_icon} alt="" />
@@ -40,7 +40,7 @@ const ProductDisplay = (props) => {
         </div>
         <div className="productdisplay-right-size">
           <h1>Select Size</h1>
-          <div className="productdisplay-right-size">
+          <div className="productdisplay-right-sizes">
             <div>S</div>
             <div>M</div>
             <div>L</div>
