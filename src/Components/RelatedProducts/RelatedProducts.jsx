a0b87ca81418f05eca10860f3b21a140252f265a
Neil Lorraine Bundoc <neillorrainecb@gmail.com>
2023-10-25 14:49:45 +0800

    Add Related Products feature

--- src/Components/RelatedProducts/RelatedProducts.jsx
+++ src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import './RelatedProducts.css'
+import data_product from '../Assets/data'
 
 const RelatedProducts = () => {
   return (
-    <div className='relatedproducts'></div>
+    <div className='relatedproducts'>
+        <h1>Related Products</h1>
+        <hr />
+        <div className="relatedproducts-item">
+            {data_product.map(()=>{
+                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+            })}
+        </div>
+    </div>
   )
 }
 
